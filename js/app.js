0a63cf165c62326d191958425efcfd78e2322229
OydinJurayeva <oydinjorayeva1020@gmail.com>
2023-07-27 12:20:34 +0500

    new

--- js/app.js
+++ js/app.js
@@ -1,66 +1,27 @@
-const api = "https://jsonplaceholder.typicode.com/todos/";
-const radioBtns = document.getElementsByName("list");
+const overlay = document.querySelector('.overlay')
+const art = document.querySelector('.article-list')
+
 
 const request = new XMLHttpRequest();
-let data;
+
 request.addEventListener("readystatechange", () => {
   if (request.readyState == 4 && request.status == 200) {
-    data = JSON.parse(request.responseText);
-    updateUi(data);
+    const data = JSON.parse(request.responseText);
+    updateUI(data);
+    overlay.classList.add('hidden')
   } else if (request.readyState == 4) {
-    console.log("Error");
+    console.log("error");
+    overlay.classList.add('hidden')
   } else {
-    console.log("Loading...");
+    overlay.classList.remove('hidden')
   }
 });
 
-request.open("get", api);
+request.open("GET", "http://localhost:3000/articles");
 request.send();
 
-radioBtns.forEach((radio) => {
-  radio.addEventListener("change", () => {
-    let select = document.querySelector("input[name='list']:checked").value;
-    res = [];
-    ul.innerHTML = "";
-    if (select == "true") {
-      res = data.filter((item) => item.completed == true);
-    } else if (select == "false") {
-      res = data.filter((item) => item.completed == false);
-    } else {
-      res = data;
-      console.log("salom", res);
-    }
-    //   console.log(res);
-    res.forEach((todo) => {
-      ul.innerHTML += `
-                <li class="card">
-                <h3>ID: ${todo.id}</h3>
-                <h4 >Completed: ${todo.completed}</h4>
-                <p>${todo.title}</p>
-                </li>
-                `;
-    });
-  });
-});
-
-let res = [];
-const ul = document.querySelector("ul");
-function updateUi(data) {
-  let select = document.querySelector("input[name='list']:checked").value;
-  if (select == "true") {
-    res = data.filter((item) => item.completed == true);
-  } else if (select == "false") {
-    res = data.filter((item) => item.completed == false);
-  } else {
-    res = data;
-  }
-  res.forEach((todo) => {
-    ul.innerHTML += `
-                <li class="card">
-                <h3>ID: ${todo.id}</h3>
-                <h4 >Completed: ${todo.completed}</h4>
-                <p>${todo.title}</p>
-                </li>
-                `;
+function updateUI(data) {
+  data.forEach((article) => {
+    console.log(article);
   });
 }
